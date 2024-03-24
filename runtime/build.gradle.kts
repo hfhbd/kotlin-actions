3176e1b2dfbfc13994a8a797f207c1042eeb87da
hfhbd <hfhbd@users.noreply.github.com>
2024-03-24 21:28:17 +0100

    Refactor to Kotlin MPP plugin

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,17 +1,24 @@
 plugins {
-    kotlin("js")
+    kotlin("multiplatform")
 }
 
 kotlin {
     js {
         nodejs()
     }
-}
 
-dependencies {
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-    api(npm("@actions/github", "6.0.0"))
-    api(npm("@actions/core", "1.10.1"))
-
-    testImplementation(kotlin("test"))
+    sourceSets {
+        named("jsMain") {
+            dependencies {
+                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+                api(npm("@actions/github", "6.0.0"))
+                api(npm("@actions/core", "1.10.1"))
+            }
+        }
+        named("jsTest") {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
 }
