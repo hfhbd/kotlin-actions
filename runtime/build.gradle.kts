b66e5d2bc12161d35aee02973c9717371fabfba2
hfhbd <hfhbd@users.noreply.github.com>
2024-03-25 18:46:41 +0100

    Add js-plain-objects

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,16 +1,26 @@
+import org.jetbrains.kotlin.gradle.dsl.*
+
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.js-plain-objects")
 }
 
 kotlin {
     js {
         nodejs()
+        useEsModules()
+
+        compilerOptions {
+            moduleKind.set(JsModuleKind.MODULE_ES)
+            useEsClasses.set(true)
+        }
     }
 
     sourceSets {
         named("jsMain") {
             dependencies {
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+                api("org.jetbrains.kotlin-wrappers:kotlin-node:20.11.30-pre.719")
                 api(npm("@actions/github", "6.0.0"))
                 api(npm("@actions/core", "1.10.1"))
             }
