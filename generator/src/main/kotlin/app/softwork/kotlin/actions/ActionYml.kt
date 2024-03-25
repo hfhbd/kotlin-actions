fd8abcd44b91469fc38fa6292841fbd6a2bf9eee
hfhbd <hfhbd@users.noreply.github.com>
2024-03-25 15:38:19 +0100

    Finish JS schema

--- generator/src/main/kotlin/app/softwork/kotlin/actions/ActionYml.kt
+++ generator/src/main/kotlin/app/softwork/kotlin/actions/ActionYml.kt
@@ -5,16 +5,24 @@ import kotlinx.serialization.*
 @Serializable
 public data class ActionYml(
     val name: String,
+    val author: String? = null,
     val description: String,
     val inputs: Map<String, Input>? = null,
     val outputs: Map<String, Output>? = null,
     val runs: Runs,
+    val branding: Branding? = null,
 )
 
 @Serializable
 public data class Runs(
     val using: Using,
+    val pre: String? = null,
+    @SerialName("pre-if")
+    val preIf: String? = null,
     val main: String,
+    val post: String? = null,
+    @SerialName("post-if")
+    val postIf: String? = null,
 )
 
 // https://nodejs.org/en/about/previous-releases
@@ -35,9 +43,43 @@ public data class Input(
     val description: String,
     val required: Boolean,
     val default: String? = null,
+    val deprecationMessage: String? = null,
 )
 
 @Serializable
 public data class Output(
     val description: String,
 )
+
+@Serializable
+public data class Branding(
+    val color: Color,
+    val icon: String,
+) {
+    @Serializable
+    public enum class Color {
+        @SerialName("white")
+        White,
+
+        @SerialName("yellow")
+        Yellow,
+
+        @SerialName("blue")
+        Blue,
+
+        @SerialName("green")
+        Green,
+
+        @SerialName("orange")
+        Orange,
+
+        @SerialName("red")
+        Red,
+
+        @SerialName("purple")
+        purple,
+
+        @SerialName("gray-dark")
+        GrayDark,
+    }
+}
