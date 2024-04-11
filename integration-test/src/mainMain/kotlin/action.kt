08fe33ff8f347fc26580261317d23db0fc50ca3c
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-04-11 12:53:12 +0200

    Use GitHub api in tests

--- integration-test/src/mainMain/kotlin/action.kt
+++ integration-test/src/mainMain/kotlin/action.kt
@@ -1,6 +1,6 @@
 import com.github.actions.github
 
 suspend fun action(token: String) {
-    val actor = github.context.actor
-    println("Hello $actor")
+    val user = github.getOctokit(token).rest.users.getAuthenticated()
+    println("Hello ${user.login}")
 }
