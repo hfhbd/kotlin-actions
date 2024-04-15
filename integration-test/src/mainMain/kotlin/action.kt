7e19f830567d2113ce6b97ff3365cb83d2667b37
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-04-15 14:24:29 +0200

    WIP

--- integration-test/src/mainMain/kotlin/action.kt
+++ integration-test/src/mainMain/kotlin/action.kt
@@ -2,6 +2,6 @@ import com.github.actions.github
 import com.github.actions.token
 
 suspend fun action() {
-    val user = github.getOctokit(github.context.token).rest.users.getAuthenticated()
+    val user = github.getOctokit("sfa").rest.users.getAuthenticated()
     println("Hello ${user.login}")
 }
