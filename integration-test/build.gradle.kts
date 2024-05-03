63382b49f90da1e9f68e2322a5de526d781f9e79
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-05-03 18:28:08 +0200

    Use GitHub api in tests (#22)

--- integration-test/build.gradle.kts
+++ integration-test/build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     id("app.softwork.kotlin.actions")
+    kotlin("plugin.serialization")
 }
 
-kotlin.sourceSets.mainTest {
-    dependencies {
-        implementation(kotlin("test"))
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+kotlin.sourceSets{
+    mainMain {
+        dependencies {
+            implementation(libs.ktor.client.js)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+        }
+    }
+    mainTest {
+        dependencies {
+            implementation(kotlin("test"))
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+        }
     }
 }
--- integration-test/src/mainMain/kotlin/action.kt
+++ integration-test/src/mainMain/kotlin/action.kt
@@ -1,6 +1,30 @@
-import com.github.actions.github
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.js.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 suspend fun action(token: String) {
-    val actor = github.context.actor
-    println("Hello $actor")
+    val client = HttpClient(Js) {
+        install(ContentNegotiation) {
+            json(
+                json = Json {
+                    ignoreUnknownKeys = true
+                    useAlternativeNames = false
+                }
+            )
+        }
+    }
+    val user = client.get("https://api.github.com/user") {
+        bearerAuth(token)
+    }.body<PublicUser>()
+    println("Hello ${user.login}")
 }
+
+@Serializable
+data class PublicUser(
+    val login: String,
+)
