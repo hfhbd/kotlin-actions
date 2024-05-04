800c0603e3db317274163f1af7f568906ce2c007
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-05-04 11:12:47 +0200

    Revert "Replace core with generated core toolkit"
    This reverts commit a100e7b6e2c1c4b8878366a8366f7ae0d38c351a.
    
    Replace core with generated core toolkit
    
    Add import with node: prefix
    
    Add externalsType
    
    Replace core with generated core
    
    Add test
    
    Expose the token env variable via the context

--- integration-test/sub/src/mainMain/kotlin/action.kt
+++ integration-test/sub/src/mainMain/kotlin/action.kt
@@ -1,3 +1,6 @@
+import com.github.actions.github
+import com.github.actions.token
+
 suspend fun action(token: String?): Outputs {
-    return Outputs(token ?: "asdf")
+    return Outputs(token ?: github.context.token)
 }
