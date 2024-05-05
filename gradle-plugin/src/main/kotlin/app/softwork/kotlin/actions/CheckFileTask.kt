fdcd12317cc36171af7b4b093df34a6c13590c8d
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-05-05 18:56:00 +0200

    Add check task (#51)
    * Add check task
    
    * Add check task
    
    * Fix task order
    
    * Checkin dist files
    
    * Checkin correct dist files

---
+++ gradle-plugin/src/main/kotlin/app/softwork/kotlin/actions/CheckFileTask.kt
@@ -0,0 +1,33 @@
+package app.softwork.kotlin.actions
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
+import org.gradle.api.tasks.Input
+import org.gradle.work.DisableCachingByDefault
+import java.io.File
+
+@DisableCachingByDefault(because = "Not worth caching")
+abstract class CheckFileTask : DefaultTask() {
+    @get:Input
+    abstract val expected: Property<String>
+
+    @get:InputFile
+    @get:PathSensitive(PathSensitivity.NONE)
+    abstract val actual: RegularFileProperty
+
+    @get:Input
+    internal abstract val copyTaskPath: Property<String>
+
+    @TaskAction
+    fun checkContent() {
+        val expected = File(expected.get())
+        require(expected.exists()) {
+            "$expected not found. Did you forget to call ${copyTaskPath.get()}?"
+        }
+        require(expected.readText() == actual.asFile.get().readText()) {
+            "$expected does not match ${actual.asFile.get()}. Did you forget to call ${copyTaskPath.get()}?"
+        }
+    }
+}
