c8075049484d8b911a9e677b2005d284ccddf73e
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-05-16 18:15:40 +0200

    Replace GitHub expressions before parsing the Json yml file

--- action-json/src/main/kotlin/app/softwork/kotlin/actions/Input.kt
+++ action-json/src/main/kotlin/app/softwork/kotlin/actions/Input.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 public data class Input(
     val description: String,
-    val required: Boolean,
+    val required: Boolean = false,
     val default: String? = null,
     val deprecationMessage: String? = null,
 )
--- integration-test/sub/src/mainMain/kotlin/action.kt
+++ integration-test/sub/src/mainMain/kotlin/action.kt
@@ -1,6 +1,6 @@
 import com.github.actions.github
 import com.github.actions.token
 
-suspend fun action(token: String?): Outputs {
+suspend fun action(token: String?, foo: String): Outputs {
     return Outputs(token ?: github.token)
 }
