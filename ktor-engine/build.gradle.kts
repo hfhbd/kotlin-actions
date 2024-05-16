0416865dcbd3af13d12c8d573b2e8ad994dea0f7
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-05-16 18:17:30 +0000

    dists

--- ktor-engine/build.gradle.kts
+++ ktor-engine/build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
         named("jsMain") {
             dependencies {
                 api(libs.ktor.client.js)
-                api("org.jetbrains.kotlin-wrappers:kotlin-node:20.11.30-pre.741")
+                api("org.jetbrains.kotlin-wrappers:kotlin-node:20.11.30-pre.747")
             }
         }
     }
