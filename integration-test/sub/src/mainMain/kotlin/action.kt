d90b208086e6c9149a3eb19e70359033684c5425
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-06-24 09:38:46 +0200

    Remove the token env to enforce passing it as input

--- integration-test/sub/src/mainMain/kotlin/action.kt
+++ integration-test/sub/src/mainMain/kotlin/action.kt
@@ -1,6 +1,5 @@
 import com.github.actions.github
-import com.github.actions.token
 
 suspend fun action(token: String?, foo: String): Outputs {
-    return Outputs(token ?: github.token)
+    return Outputs(token ?: foo)
 }
