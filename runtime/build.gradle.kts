d78703a81db124815a2e196d48271822d2c06959
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-08-03 14:18:07 +0200

    Update Kotlin Wrappers to 786

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -21,7 +21,7 @@ kotlin {
             dependencies {
                 api(libs.coroutines.core)
                 api(npm("@actions/github", "6.0.0"))
-                api("org.jetbrains.kotlin-wrappers:kotlin-actions-toolkit:0.0.1-pre.774")
+                api("org.jetbrains.kotlin-wrappers:kotlin-actions-toolkit:1.0.0-pre.786")
             }
         }
         named("jsTest") {
