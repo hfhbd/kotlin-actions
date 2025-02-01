471dfa5ed8e10717505b525fc85ae67540ef89d5
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-02-01 20:31:40 +0100

    Use custom compilations (#248)
    * WIP
    
    * AAA
    
    * AAA

--- gradle-plugin/src/main/kotlin/app.softwork.kotlin.actions.gradle.kts
+++ gradle-plugin/src/main/kotlin/app.softwork.kotlin.actions.gradle.kts
@@ -1,5 +1,7 @@
 import app.softwork.kotlin.actions.*
+import org.gradle.api.tasks.TaskProvider
 import org.jetbrains.kotlin.gradle.dsl.*
+import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsTargetDsl
 import org.jetbrains.kotlin.gradle.targets.js.ir.*
 import org.jetbrains.kotlin.gradle.targets.js.webpack.*
 
@@ -13,10 +15,12 @@ dependencies {
     workerActionDeps("app.softwork.kotlin.actions:generator:$VERSION")
 }
 
+val kotlinActionsWorkerClasspath = configurations.resolvable("kotlinActionsWorkerClasspath") {
+    extendsFrom(workerActionDeps.get())
+}
+
 val generateTypesafeAction by tasks.registering(GenerateTypesafeAction::class) {
-    this.workerClasspath.from(configurations.resolvable("kotlinActionsWorkerClasspath") {
-        extendsFrom(workerActionDeps.get())
-    })
+    this.workerClasspath.from(kotlinActionsWorkerClasspath)
 }
 
 val actionFile = providers.of(ActionYmlSource::class) {
@@ -29,110 +33,118 @@ val customWebpackConfig = tasks.register("createCustomWebpackConfig", CreateCust
     nodeVersion.set(actionFile.map { it.runs.using.version.dropLastWhile { it != '.' }.dropLast(1) })
 }
 
-val gitHubActionStepAttribute = Attribute.of("GitHubActionStep", String::class.java)
-
 kotlin {
-    fun setUpTarget(
-        name: String,
-        dependsOn: Boolean,
-        file: Provider<String>,
-    ) {
-        val dir = layout.projectDirectory.dir(file.map { it.dropLastWhile { it != '/' } })
-        val fileName = file.map { it.takeLastWhile { it != '/' } }
+    applyDefaultHierarchyTemplate()
 
-        val fullName = project.rootProject.name + if (project === project.rootProject) {
-            "-$name"
-        } else {
-            project.path.replace(':', '-') + '-' + name
+    js {
+        binaries.executable()
+        nodejs()
+        useEsModules()
+
+        compilerOptions {
+            target.set("es2015")
+            moduleKind.set(JsModuleKind.MODULE_ES)
+            useEsClasses.set(true)
         }
 
-        js(name) {
-            attributes.attribute(gitHubActionStepAttribute, name)
-            binaries.executable()
-            nodejs()
-            useEsModules()
+        fun setUpTarget(
+            name: String,
+            file: Provider<String>,
+        ) {
+            val dir = layout.projectDirectory.dir(file.map { it.dropLastWhile { it != '/' } })
+            val fileName = file.map { it.takeLastWhile { it != '/' } }
 
-            compilerOptions {
-                moduleKind.set(JsModuleKind.MODULE_ES)
-                useEsClasses.set(true)
+            val compilation = if (name == "main") {
+                this.compilations.named("main")
+            } else {
+                this.compilations.register(name) {
+                    val sourceName = name
 
-                moduleName.set(fileName)
-
-                sourceMap.set(false)
-                sourceMapEmbedSources.set(JsSourceMapEmbedMode.SOURCE_MAP_SOURCE_CONTENT_NEVER)
-            }
-
-            val executable = tasks.register(
-                "${name}Executable",
-                KotlinWebpack::class,
-                compilations.getByName("main"),
-            )
-            val sync = tasks.named("${name}ProductionExecutableCompileSync", DefaultIncrementalSyncTask::class)
-            executable.configure {
-                dependsOn(customWebpackConfig, sync)
-                mode = KotlinWebpackConfig.Mode.PRODUCTION
-                inputFilesDirectory.set(layout.dir(sync.flatMap { it.destinationDirectory }))
-                entryModuleName.set(fullName)
-                esModules.set(true)
-                outputDirectory.set(layout.buildDirectory.dir("actions/dist/$name"))
-                output.globalObject = "this"
-                mainOutputFileName.set(fileName)
-                sourceMaps = false
-                val configDir = customWebpackConfig.flatMap { it.outputDir.asFile }
-                webpackConfigApplier {
-                    configDirectory = configDir.get()
+                    compileTaskProvider {
+                        compilerOptions.moduleName.set(sourceName)
+                    }
                 }
             }
-            customWebpackConfig {
-                entry.set(executable.flatMap { it.entry })
-            }
-            tasks.register("copyAction${name}Dist", Copy::class) {
-                from(executable.flatMap { it.outputDirectory.file(fileName) })
-                into(dir)
-            }
-            val expectedWithoutTasksDependencyToNotRunCopy = objects.fileProperty().apply {
-                set(dir.flatMap { it.file(fileName) })
-            }.locationOnly.map { it.asFile.absolutePath }
-            val checkDist = tasks.register("check${name}Dist", CheckFileTask::class) {
-                actual.set(executable.flatMap { it.outputDirectory.file(fileName) })
-                expected.set(expectedWithoutTasksDependencyToNotRunCopy)
-                copyTaskPath.set(path.dropLastWhile { it != ':' } + "copyAction${name}Dist")
-            }
+            binaries.executable(compilation.get())
+            compilation {
+                val executable = tasks.register(
+                    "${name}Executable",
+                    KotlinWebpack::class,
+                    this,
+                )
 
-            tasks.assemble {
-                dependsOn(executable)
-            }
-            tasks.check {
-                dependsOn(checkDist)
+                val sync: TaskProvider<out org.jetbrains.kotlin.gradle.tasks.IncrementalSyncTask> = if (name == "main") {
+                    tasks.named("jsProductionExecutableCompileSync", DefaultIncrementalSyncTask::class)
+                } else {
+                    val bug = name.replaceFirstChar { it.uppercaseChar() }
+                    tasks.named("js${bug}${bug}ProductionExecutableCompileSync", DefaultIncrementalSyncTask::class)
+                }
+
+                val fullName = outputModuleName
+
+                executable.configure {
+                    dependsOn(customWebpackConfig, sync)
+                    mode = KotlinWebpackConfig.Mode.PRODUCTION
+                    inputFilesDirectory.set(layout.dir(sync.flatMap { it.destinationDirectory }))
+                    entryModuleName.set(fullName)
+                    esModules.set(true)
+                    outputDirectory.set(layout.buildDirectory.dir("actions/dist/$name"))
+                    output.globalObject = "this"
+                    mainOutputFileName.set(fileName)
+                    sourceMaps = false
+                    val configDir = customWebpackConfig.flatMap { it.outputDir.asFile }
+                    webpackConfigApplier {
+                        configDirectory = configDir.get()
+                    }
+                }
+                tasks.register("copyAction${name}Dist", Copy::class) {
+                    from(executable.flatMap { it.outputDirectory.file(fileName) })
+                    into(dir)
+                }
+                val expectedWithoutTasksDependencyToNotRunCopy = objects.fileProperty().apply {
+                    set(dir.flatMap { it.file(fileName) })
+                }.locationOnly.map { it.asFile.absolutePath }
+                val checkDist = tasks.register("check${name}Dist", CheckFileTask::class) {
+                    actual.set(executable.flatMap { it.outputDirectory.file(fileName) })
+                    expected.set(expectedWithoutTasksDependencyToNotRunCopy)
+                    copyTaskPath.set(path.dropLastWhile { it != ':' } + "copyAction${name}Dist")
+                }
+
+                tasks.assemble {
+                    dependsOn(executable)
+                }
+                tasks.check {
+                    dependsOn(checkDist)
+                }
+
+                defaultSourceSet {
+                    if (name == "main") {
+                        kotlin.srcDirs(generateTypesafeAction)
+                    } else {
+                        dependsOn(this@kotlin.sourceSets.getByName("commonMain"))
+                    }
+                    dependencies {
+                        implementation("app.softwork.kotlin.actions:runtime:$VERSION")
+                    }
+                }
             }
         }
-        sourceSets {
-            named("${name}Main") {
-                if (dependsOn) {
-                    kotlin.srcDirs(generateTypesafeAction)
-                }
-                dependencies {
-                    implementation("app.softwork.kotlin.actions:runtime:$VERSION")
-                }
-            }
+
+        setUpTarget("main", actionFile.map { it.runs.main })
+
+        val runs = actionFile.map { it.runs }
+        val pre: Provider<String> = runs.map { it.pre }
+        if (pre.isPresent) {
+            setUpTarget("pre", pre)
         }
-    }
-
-    setUpTarget("main", true, actionFile.map { it.runs.main })
-
-    val runs = actionFile.map { it.runs }
-    val pre: Provider<String> = runs.map { it.pre }
-    if (pre.isPresent) {
-        setUpTarget("pre", false, pre)
-    }
-    val post: Provider<String> = runs.map { it.post }
-    if (post.isPresent) {
-        setUpTarget("post", false, post)
+        val post: Provider<String> = runs.map { it.post }
+        if (post.isPresent) {
+            setUpTarget("post", post)
+        }
     }
 }
 
-rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsEnvSpec> {
     // https://youtrack.jetbrains.com/issue/KT-65639
     version = actionFile.map { it.runs.using.version }.get()
-    versions.webpack.version = "5.94.0"
 }
